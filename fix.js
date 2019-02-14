cf97f350a8155af2a4cd1eb930db70c49a9a9ff0
Zordius Chen <1201409+zordius@users.noreply.github.com>
2019-02-14 15:41:05 +0800

    add fix.js for gopro gpx

---
+++ fix.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+const { promisify } = require('util')
+const { writeFileSync } = require('fs')
+const gpsUtil = require('gps-util')
+const Victor = require('victor')
+
+const [ , myname, file ] = process.argv
+
+const gpxParseFile = promisify(gpsUtil.gpxParseFile)
+const toGPX = promisify(({ points, title }, callback) => gpsUtil.toGPX({ points }, callback, title))
+
+const addSecond = trackpoint => {
+  trackpoint.second = (new Date(trackpoint.time)).getTime() / 1000
+  return trackpoint
+}
+
+const addSeconds = trackpoints => trackpoints.map(trackpoint => addSecond(trackpoint))
+
+const filterGoproBadTime = trackpoints => {
+  let prev = trackpoints[0]
+  return trackpoints.filter(cur => {
+    const OK = cur.time > prev.time && !((cur.lat === prev.lat) && (cur.lng === prev.lng))
+    prev = cur
+    return OK
+  })
+}
+
+// Usage
+if (!file) {
+  console.warn(`
+Usage: ${myname} gpxfile
+`)
+  process.exit()
+}
+
+gpxParseFile(file)
+.then(addSeconds)
+.then(filterGoproBadTime)
+.then(trackpoints => toGPX({
+  points: trackpoints,
+  title: file
+}))
+.then(data => writeFileSync(`${file}.fixed.gpx`, data.replace(/<trkpt/g, '\n<trkpt')))
